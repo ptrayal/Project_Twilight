124d0d6fdcc8588e4806e88d62e041c9f673a64b
Brandon <ptrayal@gmail.com>
2016-10-25 10:11:35 -0500

    [Code] Change value for array checking

--- src/act_info.c
+++ src/act_info.c
@@ -6054,8 +6054,9 @@ void do_snippets( CHAR_DATA *ch, char *argument )
 
 void do_testarray (CHAR_DATA *ch, char *argument)
 {
+	OBJ_DATA *obj
 	CheckCH(ch);
 
-	send_to_char( Format("Length of ch->armor: %d\n\r", (int)( sizeof(ch->armor) / sizeof(ch->armor[0]) )), ch);
+	send_to_char( Format("Length of obj->value: %d\n\r", (int)( sizeof(obj->value) / sizeof(obj->value[0]) )), ch);
 
 }
